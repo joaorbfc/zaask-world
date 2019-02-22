24bc1dceb00e91605655f252d2f3cbc19139617d
João Cardoso (1160665) <1160665@isep.ipp.pt>
2019-02-22 12:48:22 +0000

    Added CountryControllers with the requests implementation

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Country;
+use App\Models\Countrylanguage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CountryController extends Controller
+{
+    public function getCountryByCode($code){
+       $lang = Countrylanguage::where('CountryCode','=', $code)->get();
+        $country = Country::findOrFail($code);
+        return response()->json([
+            'Country' => $country,
+            'Lang' => $lang
+        ]);
+    }
+
+    public function updateHS(Request $request, $code){
+        $country = Country::findOrFail($code);
+        $validation = Validator::make($request->all(),[
+            'HeadOfState'=>'required'
+        ]);
+        if($validation->fails()){
+            return response()->json([
+                'success' => false,
+                'Message'=>'Invalid data'
+            ]);
+        }else{
+            $country->HeadOfState = $request->HeadOfState;
+            $country->save();
+            return response()->json([
+                'success' => true,
+                'Message'=>'Updated successfully'
+            ]);
+        }
+    }
+}
